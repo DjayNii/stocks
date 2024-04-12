2cc5e1f50ed1842aba2f575712cfa72cf2c253db
DjayNii <nithinreddyk24@gmail.com>
2024-04-12 20:36:30 +0530

    displays rsi

--- index.js
+++ index.js
@@ -15,15 +15,22 @@ async function gettingData() {
       `https://www.alphavantage.co/query?function=RSI&symbol=${symbol}&interval=monthly&time_period=10&series_type=open&apikey=70N9808NO57B3S0I`
     );
 
+    const responce3 = await fetch(
+      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=70N9808NO57B3S0I`
+    );
+
     if (!response.ok) {
       throw new Error(`error in getting deatils `);
     }
 
     const data = await response.json();
     const rsiData = await responce2.json();
+    const dayChange = await responce3.json();
 
     console.log(data);
     console.log(rsiData);
+    console.log(dayChange);
+
     const stockData = data.bestMatches[0];
 
     const dates = rsiData["Technical Analysis: RSI"];
@@ -36,8 +43,20 @@ async function gettingData() {
 
     const stockName = stockData["2. name"];
 
-    infoContainer.innerHTML = `<p> Name : ${stockName}</p>
-    <p>latest rsi : ${rsi}</p>`;
+    const changePercent = dayChange["Global Quote"]["10. change percent"];
+    const symbolNAME = dayChange["Global Quote"]["01. symbol"];
+    const price = dayChange["Global Quote"]["08. previous close"];
+
+    console.log(changePercent);
+
+    infoContainer.innerHTML = `<p>${stockName}</p>
+    <p> ${symbolNAME} </p>
+    <p> ${price} </p>
+    <p> ${changePercent}</p>
+    <p>RSI Value</p> 
+    <p> ${rsi}</p>`;
+
+    infoContainer.style.display = "flex";
   } catch (error) {
     console.log(error);
   }
